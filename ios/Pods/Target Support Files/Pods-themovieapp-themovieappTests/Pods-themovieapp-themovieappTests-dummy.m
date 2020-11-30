7b7d2cde815480a56e0f7783b6e229d6bec0560f
Edgar Salgado <poncho.12@hotmail.com>
2020-11-29 20:55:39 -0600

    commit

---
+++ ios/Pods/Target Support Files/Pods-themovieapp-themovieappTests/Pods-themovieapp-themovieappTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_themovieapp_themovieappTests : NSObject
+@end
+@implementation PodsDummy_Pods_themovieapp_themovieappTests
+@end
---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const API_HOST = 'https://api.themoviedb.org/3';
+export const API_KEY = 'e570e85ca9070b090594576d7d57d9f1';
+export const LANG = 'es-ES';
+export const BASE_PATH_IMG = 'https://image.tmdb.org/t/p';
